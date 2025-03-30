d7c6b5504b1a70dc53fd16c0a7cc2fd4c6418bc3
eyagi <eyagi@students.cs.ubc.ca>
2025-03-29 21:33:18 -0700

    adding past note page

--- frontend/src/components/PastNotes/PastNotes.jsx
+++ frontend/src/components/PastNotes/PastNotes.jsx
@@ -1,7 +1,23 @@
+import { useEffect, useState } from "react";
+import { axios } from "axios";
 import "./PastNotes.scss";
 
-function VerticalNavBar() {
 
+function PastNotes() {
+    const [myNotes, setMyNotes] = useState([]);
+
+    // useEffect(() => {
+    //     const getAllPosts = () => {
+    //       axios
+    //         .get(`http://localhost:8080/posts/`)
+    //         .then((response) => {
+    //           setMyPosts(response.data);
+    //           console.log(response.data);
+    //         })
+    //         .catch((error) => console.log(error));
+    //     };
+    //     getAllPosts();
+    //   }, []);
 
     return(
      <>
@@ -12,4 +28,4 @@ function VerticalNavBar() {
     )
 }
 
-export default VerticalNavBar;
+export default PastNotes;
